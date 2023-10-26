bb16988f2c6f7f41e99b9f068b59cf3f3d3bd57f
EstelaLacerda <elo@cesar.school>
2023-10-26 11:39:19 -0300

    faltava a vírgula, adicionei

--- eden_project/eden_project/settings.py
+++ eden_project/eden_project/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['templates'],
-        'APP_DIRS': True
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
