f05fed65a7a4cfa48d70159772ebbde9ef397283
SofiaValadares <svc3@cesar.school>
2023-10-27 11:57:07 -0300

    classe Midia e PerfilColaborador em Models

--- eden_project/gestor/models.py
+++ eden_project/gestor/models.py
@@ -11,41 +11,34 @@ class Midia(models.Model):
     )
 
     titulo = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name='user_list', on_delete=models.CASCADE)
     descricao = models.TextField(default="Escreva sua descrição", max_length= 150, blank= True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='analise')
     dataPostagem = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, related_name='user_list', on_delete=models.CASCADE)
-    ## cartas
-    ## filme player
+    ## argMidia
+    ## arqCartaz
 
     def __str__(self):
         return self.titulo
 
 
 class PerfilColaborador(models.Model):
+    CARGO_CHOICES = (
+        ('masteruser', 'MasterUser'),
+        ('reportuser', 'ReportUser'),
+        ('midiauser', 'MidiaUser'),
+    )
+
+    STATUS_CHOICES = (
+        ('analise', 'Em_Analise'),
+        ('aprovado', 'Aprovado'),
+        ('reprovado', 'Reprovado'),
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ## cargos
-    ## textoMotivacao
-    ## status
+    cargo = models.CharField(max_length=10, choices=STATUS_CHOICES, default='analise')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='analise')
+    atividade = models.BooleanField(default="True")
 
     def __str__(self):
         return self.user
-    
-
-class UserRequestePerfilColaborador(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cargo = models.CharField(max_length=100)
-    textoMotivacao = models.TextField(default="Porque você quer ser coloborador?", max_length= 150, blank= True)
-
-    def __str__(self):
-        return self.user
-
-#class FomularioSuporte(models.Model):
-#    user = models.ForeignKey(User, related_name='user_list', on_delete=models.CASCADE, blank= True, null= True)
-#    titulo = models.CharField(max_length=100)
-    ## status ou colaborador
-
-#class MensagemFomularioSuporte(models.Model):
-#    user = models.ForeignKey(User, related_name='user_list', on_delete=models.CASCADE, blank= True, null= True)
-    ## mensagem
-    ## dataMensagem
