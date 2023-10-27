1cf1913c5f8aed683e4ec76feae6efb41964adcc
EstelaLacerda <elo@cesar.school>
2023-10-27 12:21:44 -0300

    Correção de campos em admin.py

--- eden_project/gestor/admin.py
+++ eden_project/gestor/admin.py
@@ -3,14 +3,14 @@ from .models import PerfilColaborador, Midia, UserHistorico
 
 # Register your models here.
 class ListandoMidia(admin.ModelAdmin):
-    list_display = ("id", "titulo", "user", "status")
+    list_display = ("id", "titulo", "user", "status", "dataPostagem")
     list_display_links =("id", "titulo")
     search_fields = ("titulo", "user",)
 
 admin.site.register(Midia, ListandoMidia)
 
 class ListandoPerfilColaborador(admin.ModelAdmin):
-    list_display = ("id", "user")
+    list_display = ("id", "user", "cargos", "status","atividade")
     list_display_links =("id", "user")
     search_fields = ("user",)
 
