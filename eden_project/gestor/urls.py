ffcc0c5515b4441d1ab44117bf10c44973f9c777
SofiaValadares <svc3@cesar.school>
2023-10-30 18:48:22 -0300

    html basico

--- eden_project/gestor/urls.py
+++ eden_project/gestor/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from gestor.views import home
 
 
 urlpatterns = [
+    path('', home, name='home'),
     
 ]
--- eden_project/gestor/views.py
+++ eden_project/gestor/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    request.use=None
+    return render(request, 'home.html')
