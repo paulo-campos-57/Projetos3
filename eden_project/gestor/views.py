168137948a52b310a8ebe557831678804527366d
SofiaValadares <svc3@cesar.school>
2023-11-07 14:27:36 -0300

    modenca veiws home

--- eden_project/gestor/views.py
+++ eden_project/gestor/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from gestor.models import PerfilColaborador
 
 # Create your views here.
 
 def home(request):
-    request.use=None
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        try:
+            perfil_colaborador = PerfilColaborador.objects.get(user=request.user)
+        except PerfilColaborador.DoesNotExist:
+            perfil_colaborador = None
+        
+        return render(request, 'home.html', {'perfil_colaborador' : perfil_colaborador})
+
+    else:
+        return render(request, 'home.html')
 
 def login(request):
     request.use=None
