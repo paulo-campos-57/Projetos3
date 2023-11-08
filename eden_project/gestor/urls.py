ffb12e9a98f59247460df892e3a536e42033cf1b
paulo <paulo.m.campos6601@gmail.com>
2023-11-08 14:14:34 -0300

    Cadastro de usuário no banco de dados

--- eden_project/gestor/urls.py
+++ eden_project/gestor/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from gestor.views import home, login, cadastro, user_menu, gestao_equipe, colaboradores
+from gestor.views import home, login, cadastro, user_menu, gestao_equipe, colaboradores, usuario_cadastrado
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login, name='login'),
     path('login/cadastro', cadastro, name='cadastro'),
+    path('cadastro/efetuado', usuario_cadastrado, name='usuario_cadastrado'),
     path('user/', user_menu, name='user_menu'),
     path('gestao_equipe/', gestao_equipe, name='gestao_equipe'),
     path('colaboradores/', colaboradores, name='colaboradores'),
--- eden_project/gestor/views.py
+++ eden_project/gestor/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from gestor.models import PerfilColaborador
 
 # Create your views here.
@@ -22,6 +24,17 @@ def login(request):
 def cadastro(request):
     request.use=None
     return render(request, 'cadastro.html')
+
+def usuario_cadastrado(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # Verificar se o usuário já existe no banco de dados
+        if User.objects.filter(username = username).exists():
+            return HttpResponse('Este usuário já está cadastrado em nosso Banco de dados')
+        user = User.objects.create_user(username, email, password)
+        return render(request, 'usuario_cadastrado.html')
 
 def user_menu(request):
     return render(request, 'user.html')
