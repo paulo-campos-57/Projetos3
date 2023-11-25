49ff0a9f54d88a52ba5b20d64b06c67457e560f1
SofiaValadares <svc3@cesar.school>
2023-11-25 08:08:15 -0300

    navbar

--- eden_project/gestor/urls.py
+++ eden_project/gestor/urls.py
@@ -4,18 +4,18 @@ from gestor.views import home, login, logout_logic, cadastro, user_menu, gestao_
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login/', login, name='login'),
+    path('login', login, name='login'),
     path('login/cadastro', cadastro, name='cadastro'),
-    path('logout/', logout_logic, name='logout'),
+    path('logout', logout_logic, name='logout'),
     path('cadastro/efetuado', usuario_cadastrado, name='usuario_cadastrado'),
-    path('user/', user_menu, name='user_menu'),
-    path('gestao_equipe/', gestao_equipe, name='gestao_equipe'),
-    path('colaboradores/', colaboradores, name='colaboradores'),
-    path('enviar_mensagem/', enviar_mensagem, name='enviar_mensagem'),
-    path('formulario_colaborador/', formulario_colaborador, name='formulario_colaborador'),
-    path('colaborador/', colaborador, name='colaborador'),
-    path('home/', homeMasterUser, name='homeMasterUser'),
-    path('novos_membros/', novos_membros, name='novos_membros'),
-    path('novos_membros/formulario/', novos_membros_formulario, name='novos_membros_formulario'),
-    path('novos_membros/buscar/', novos_membros_buscar, name='novos_membros_buscar')
+    path('user', user_menu, name='user_menu'),
+    path('gestao_equipe', gestao_equipe, name='gestao_equipe'),
+    path('colaboradores', colaboradores, name='colaboradores'),
+    path('enviar_mensagem', enviar_mensagem, name='enviar_mensagem'),
+    path('formulario_colaborador', formulario_colaborador, name='formulario_colaborador'),
+    path('colaborador', colaborador, name='colaborador'),
+    path('home', homeMasterUser, name='homeMasterUser'),
+    path('novos_membros', novos_membros, name='novos_membros'),
+    path('novos_membros/formulario', novos_membros_formulario, name='novos_membros_formulario'),
+    path('novos_membros/buscar', novos_membros_buscar, name='novos_membros_buscar')
 ]
